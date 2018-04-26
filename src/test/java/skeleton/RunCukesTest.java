bff5a620f07a1cc1c1f556bc5b404a49b5900336
lukasz <lukasz.jalowiecki@gmail.com>
2018-04-26 21:50:27 +0200

    próba uruchomienia klasy testowej

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -2,9 +2,18 @@ package skeleton;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty", "io.cucumber.pro.JsonReporter:all"})
 public class RunCukesTest {
+	
+	@Test
+	public void test() {
+	    assertEquals(15, 15);
+	}
 }
