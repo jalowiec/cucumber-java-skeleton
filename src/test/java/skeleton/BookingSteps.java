23803f1c86b9af76c6b6f083b33efda37a2f2718
lukasz <lukasz.jalowiecki@gmail.com>
2018-05-16 11:08:33 +0200

    Poprawa scenariusza book free room - usuniecie Arity mismatch

--- src/test/java/skeleton/BookingSteps.java
+++ src/test/java/skeleton/BookingSteps.java
@@ -7,33 +7,39 @@ import cucumber.api.java.en.Then;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 
 public class BookingSteps {
 
 	
 	HotelManager hotel = new HotelManager();
+	Person bookingPerson = new Person(1, "Jan", "Kowalski");
+	int bookingId = 100;
+	Room bookingRoom;
+	Booking booking;
 	
-	
-
 	@Given("^there is a free room$")
 	public void there_is_a_free_room() {
 		hotel.addRoom();
+		bookingRoom = hotel.getFreeRoom();
 	}
 
 	@When("^person A books it$")
-	public void person_A_books_it(Person bookingPerson, Room bookingRoom) {
-	//	hotel.bookingRoom(bookingPerson, bookingRoom);	
+	public void person_A_books_it() {
+		hotel.bookingRoom(bookingId, bookingPerson, bookingRoom);
+		booking = hotel.getBookingFromList(bookingId);
 	}
 
 	@Then("^it is booked by person A$")
-	public void it_is_booked_by_personA(Person bookingPerson, Booking booking) {
+	public void it_is_booked_by_person_A() {
 		assertEquals(bookingPerson, booking.getBookingPerson() );
 	}
 
+	@Then("^it can no longer be booked$")
+	public void it_can_no_longer_be_booked() {
+		assertFalse(bookingRoom.isFree());
+	}	
 	
-	@And("^it can no longer be booked")
-	public void it_can_no_longer_be_booked(Room room) {
-		assertFalse(room.isFree());
-	}
+
 }
